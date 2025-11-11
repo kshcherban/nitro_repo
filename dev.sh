dea4c7711c65f135d64394945cc188dd4ebfb3bb
kshcherban <insider@sudoers.dev>
2025-11-11 16:30:09 +0100

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -13,12 +13,10 @@ set -ex
 
 # Build frontend unless -b flag is provided
 if [[ "$SKIP_FRONTEND" = false ]]; then
-    cd site
-    npm run build
-    cd ../
-    cargo build --features frontend
-else
-    cargo build --features frontend
+  export VITE_BASE_URL=/
+  npm --prefix site run build-only -- --mode development --minify false --sourcemap true
 fi
+
+cargo build --features frontend
 
 docker compose -f docker-compose.yml -f docker-compose.dev.yml up -d --force-recreate nitro_repo
